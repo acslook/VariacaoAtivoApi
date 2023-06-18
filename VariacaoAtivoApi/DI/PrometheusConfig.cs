4da711295435680ba1e5d0e53a9751495190b1dd
Adriano Candido da Silva <acslook@hotmail.com>
2023-06-17 21:02:24 -0300

    add prometheus config

---
+++ VariacaoAtivoApi/DI/PrometheusConfig.cs
@@ -0,0 +1,35 @@
+﻿using Prometheus;
+
+namespace VariacaoAtivoApi.DI
+{
+    public static class PrometheusConfig
+    {
+
+        public static IApplicationBuilder UsePrometheusConfiguration(this IApplicationBuilder app)
+        {
+            /*INICIO DA CONFIGURAÇÃO - PROMETHEUS*/
+
+            // Custom Metrics to count requests for each endpoint and the method
+            var counter = Metrics.CreateCounter("webapimetric", "Counts requests to the WebApiMetrics API endpoints",
+                new CounterConfiguration
+                {
+                    LabelNames = new[] { "method", "endpoint" }
+                });
+
+            app.Use((context, next) =>
+            {
+                counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+                return next();
+            });
+
+            // Use the prometheus middleware
+            app.UseMetricServer();
+            app.UseHttpMetrics();
+
+            /*FIM DA CONFIGURAÇÃO - PROMETHEUS*/
+
+            return app;
+
+        }
+    }
+}
--- VariacaoAtivoApi/Program.cs
+++ VariacaoAtivoApi/Program.cs
@@ -1,3 +1,5 @@
+using Prometheus;
+using VariacaoAtivoApi;
 using VariacaoAtivoApi.DI;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.RegisterServices();
 
+builder.Services.AddHealthChecks()
+    // Define a sample health check that always signals healthy state.
+    .AddCheck<SampleHealthCheck>(nameof(SampleHealthCheck))
+    // Report health check results in the metrics output.
+    .ForwardToPrometheus();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,5 +30,25 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRouting();
+
+app.UsePrometheusConfiguration();
+
+app.UseEndpoints(endpoints =>
+{
+    // Enable the /metrics page to export Prometheus metrics.
+    // Open http://localhost:5099/metrics to see the metrics.
+    //
+    // Metrics published in this sample:
+    // * built-in process metrics giving basic information about the .NET runtime (enabled by default)
+    // * metrics from .NET Event Counters (enabled by default, updated every 10 seconds)
+    // * metrics from .NET Meters (enabled by default)
+    // * metrics about requests made by registered HTTP clients used in SampleService (configured above)
+    // * metrics about requests handled by the web app (configured above)
+    // * ASP.NET health check statuses (configured above)
+    // * custom business logic metrics published by the SampleService class
+    endpoints.MapMetrics();
+});
 
 app.Run();
